e618b54408fb524f969632ceb4af5d65c9fd357c
Tim Brünjes <brueni@fun-fm.de>
2020-01-29 18:15:50 +0100

    Add files via upload

---
+++ assets/js/lfmplayer.js
@@ -0,0 +1,34 @@
+function $_GET(param) {
+    var vars = {};
+    window.location.href.replace( location.hash, '' ).replace(
+        /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+        function( m, key, value ) { // callback
+            vars[key] = value !== undefined ? value : '';
+        }
+    );
+
+    if ( param ) {
+        return vars[param] ? vars[param] : null;
+    }
+    return vars;
+}
+
+$(
+    function(){
+        var aud = $('audio')[0];
+        aud.src ='https://stream.laut.fm/'+station;
+        aud.title = sender+' - LIVE';
+        $('.playbtn').on('click', function(){
+            if (aud.paused) {
+                aud.play();
+                $('.play-pause').removeClass('fa-play');
+                $('.play-pause').addClass('fa-stop');
+            }
+            else {
+                aud.pause();
+                $('.play-pause').removeClass('fa-stop');
+                $('.play-pause').addClass('fa-play');
+            }
+
+        })
+})
