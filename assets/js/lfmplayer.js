771492537a32f081be50662677af3173f22cbee6
Lapotor <lapotor@lapotor.de>
2020-01-29 20:24:42 +0100

    First attempt for tracklist.

--- assets/js/lfmplayer.js
+++ assets/js/lfmplayer.js
@@ -1,34 +1,33 @@
 function $_GET(param) {
-    var vars = {};
-    window.location.href.replace( location.hash, '' ).replace(
+    let vars = {};
+    window.location.href.replace(location.hash, '').replace(
         /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
-        function( m, key, value ) { // callback
+        function (m, key, value) { // callback
             vars[key] = value !== undefined ? value : '';
         }
     );
 
-    if ( param ) {
+    if (param) {
         return vars[param] ? vars[param] : null;
     }
     return vars;
 }
 
 $(
-    function(){
-        var aud = $('audio')[0];
-        aud.src ='https://stream.laut.fm/'+station;
-        aud.title = sender+' - LIVE';
-        $('.playbtn').on('click', function(){
+    function () {
+        let aud = $('audio')[0];
+        aud.src = 'https://stream.laut.fm/' + station;
+        aud.title = sender + ' - LIVE';
+        $('.playbtn').on('click', function () {
             if (aud.paused) {
                 aud.play();
                 $('.play-pause').removeClass('fa-play');
                 $('.play-pause').addClass('fa-stop');
-            }
-            else {
+            } else {
                 aud.pause();
                 $('.play-pause').removeClass('fa-stop');
                 $('.play-pause').addClass('fa-play');
             }
 
         })
-})
+    })
