7e85d0f9e8b9a22c0aa8c4431206c6c49b6e16ac
Lapotor <lapotor@lapotor.de>
2020-01-30 04:01:33 +0100

    Added Jingle/Music tag to popover
    Signed-off-by: Lapotor <lapotor@lapotor.de>

--- assets/js/tracklist.js
+++ assets/js/tracklist.js
@@ -1,4 +1,4 @@
-function appendTemplate(append_selector, track_nr, artist, title) {
+function appendTemplate(append_selector, track_nr, artist, title, jingle = false) {
     let appended = $(append_selector).append(
         $("#song_detailed").html()
             .replace("{{track_nr}}", track_nr)
@@ -9,11 +9,16 @@ function appendTemplate(append_selector, track_nr, artist, title) {
         $(this).attr("title", artist)
     });
     appended.find('[data-content*="{{title}}"]').each(function () {
+        if (jingle) {
+            title += "<br /><span class=\"badge badge-pill badge-info\">Jingle</span>";
+        } else {
+            title += "<br /><span class=\"badge badge-pill badge-danger\">Musik</span>";
+        }
         $(this).attr("data-content", title)
     })
 }
 
-function prependTemplate(prepend_selector, track_nr, artist, title) {
+function prependTemplate(prepend_selector, track_nr, artist, title, jingle = false) {
     let prepended = $(prepend_selector).prepend(
         $("#song_detailed").html()
             .replace("{{track_nr}}", track_nr)
@@ -24,6 +29,11 @@ function prependTemplate(prepend_selector, track_nr, artist, title) {
         $(this).attr("title", artist)
     });
     prepended.find('[data-content*="{{title}}"]').each(function () {
+        if (jingle) {
+            title += "<br /><span class=\"badge badge-pill badge-info\">Jingle</span>";
+        } else {
+            title += "<br /><span class=\"badge badge-pill badge-danger\">Musik</span>";
+        }
         $(this).attr("data-content", title)
     })
 }
@@ -42,9 +52,13 @@ function load_last_songs_fully(station_name) {
         last_song_data_cache = full_data.slice(0);
         full_data.shift();
         for (const [key, value] of Object.entries(full_data)) {
-            appendTemplate("#song_row", parseInt(key, 10) + 1, value.artist.name, value.title);
+            appendTemplate("#song_row", parseInt(key, 10) + 1, value.artist.name, value.title, value.type === "jingle");
         }
     });
+    $('[data-toggle="popover"]').popover({
+        trigger: 'focus',
+        html: true
+    })
 }
 
 function load_last_song(station_name) {
@@ -56,8 +70,12 @@ function load_last_song(station_name) {
             $("#song_row").children().each(function () {
                 $(".track_nr", this).text(parseInt($(".track_nr", this).text(), 10) + 1);
             });
-            prependTemplate("#song_row", 1, part_data[0].artist.name, part_data[0].title);
+            prependTemplate("#song_row", 1, part_data[0].artist.name, part_data[0].title, part_data[0].type === "jingle");
 
         }
     });
+    $('[data-toggle="popover"]').popover({
+        trigger: 'focus',
+        html: true
+    })
 }
