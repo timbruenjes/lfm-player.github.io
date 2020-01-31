543bca6fe78fc6dcdfb6a1732d0a5ad4885d2ca9
Lapotor <lapotor@lapotor.de>
2020-01-31 05:37:15 +0100

    🖼️ Added dynamic favicon
    Signed-off-by: Lapotor <lapotor@lapotor.de>

--- assets/js/lfmplayer.js
+++ assets/js/lfmplayer.js
@@ -13,6 +13,67 @@ function $_GET(param) {
     return vars;
 }
 
+function changeFavicon(apiData) {
+    document.head || (document.head = document.getElementsByTagName('head')[0]);
+    var link = document.createElement('link'),
+        oldLink = document.getElementById('dynamic-favicon');
+    link.id = 'dynamic-favicon';
+    link.rel = 'shortcut icon';
+    link.href = apiData.images.station;
+    if (oldLink) {
+        document.head.removeChild(oldLink);
+    }
+    document.head.appendChild(link);
+}
+
+function setDisplayName(apiData) {
+    $("#api_lfm_display_name").html(apiData.display_name);
+}
+
+function setCurrentPlaylist(apiData) {
+    $("#show_start").html(apiData.current_playlist.hour);
+    $("#show_name").html(apiData.current_playlist.name);
+    $("#show_description").html(apiData.current_playlist.description);
+}
+
+function setNextPlaylist(apiData) {
+    $("#next_start").html(apiData.next_playlist.hour);
+    $("#next_name").html(apiData.next_playlist.name);
+    $("#next_description").html(apiData.next_playlist.description);
+}
+
+function setStationInformation(apiData) {
+    $("#station_logo").attr('src', apiData.images.station);
+    $("#station_displayname").html(apiData.display_name);
+    $("#station_slogan").html(apiData.format);
+    $("#station_description").html(apiData.description);
+    apiData.website ? $("#hp-link").attr('href', apiData.website) : $("#hp-link").hide();
+    apiData.facebook_page ? $("#fb-link").attr('href', apiData.facebook_page) : $("#fb-link").hide();
+    apiData.twitter_name ? $("#tw-link").attr('href', "https://twitter.com/" + apiData.twitter_name) : $("#tw-link").hide();
+    apiData.third_parties.instagram ? $("#ig-link").attr('href', "https://instagram.com/" + apiData.third_parties.instagram.name) : $("#ig-link").hide();
+    $("#radiode-link").attr('href', apiData.third_parties.radiode.url);
+    $("#phonostar-link").attr('href', apiData.third_parties.phonostar.url);
+    $("#m3u-link").attr('href', apiData.stream_url + ".m3u");
+    $("#pls-link").attr('href', apiData.stream_url + ".pls");
+}
+
+function crawleLautApi(station_name) {
+    $.getJSON("https://api.laut.fm/station/" + station_name, function (data) {
+        setDisplayName(data);
+        changeFavicon(data);
+        station_info && setStationInformation(data);
+        current_show && setCurrentPlaylist(data);
+        next_show && setNextPlaylist(data);
+    });
+}
+
+function crawlePlaylist(station_name) {
+    $.getJSON("https://api.laut.fm/station/" + station_name, function (data) {
+        current_show && setCurrentPlaylist(data);
+        next_show && setNextPlaylist(data);
+    });
+}
+
 function getSong(station_name) {
     $.getJSON("https://api.laut.fm/station/" + station_name + "/current_song", function (current) {
         if (song_str !== (current.title + " " + current.artist.name)) {
@@ -22,62 +83,24 @@ function getSong(station_name) {
     })
 }
 
-function getDisplayName(station_name) {
-    $.getJSON("https://api.laut.fm/station/" + station, function (data) {
-        $("#api_lfm_display_name").html(data.display_name);
-    })
-}
-
-function getShowInformation(station_name) {
-    $.getJSON("https://api.laut.fm/station/" + station, function (data) {
-        $("#show_start").html(data.current_playlist.hour);
-		$("#show_name").html(data.current_playlist.name);
-		$("#show_description").html(data.current_playlist.description);
-		$("#next_start").html(data.next_playlist.hour);
-		$("#next_name").html(data.next_playlist.name);
-		$("#next_description").html(data.next_playlist.description);
-    })
-}
-
-function getStationInformation(station_name) {
-    $.getJSON("https://api.laut.fm/station/" + station, function (data) {
-        $("#station_logo").attr('src', data.images.station);
-		$("#station_displayname").html(data.display_name);
-		$("#station_slogan").html(data.format);
-		$("#station_description").html(data.description);
-		$("#hp-link").attr('href', data.website);
-		$("#fb-link").attr('href', data.facebook_page);
-		$("#tw-link").attr('href', "https://twitter.com/" + data.twitter_name);
-		$("#ig-link").attr('href', "https://instagram.com/" + data.third_parties.instagram.name);
-		$("#radiode-link").attr('href', data.third_parties.radiode.url);
-		$("#phonostar-link").attr('href', data.third_parties.phonostar.url);
-		$("#m3u-link").attr('href', data.stream_url + ".m3u");
-		$("#pls-link").attr('href', data.stream_url + ".pls");
-    })
-}
-
 function getWidth() {
     let wrapper = $("#nowplaying").width()
     let playing = $("#api_lfm_display_name").width() + $("#nowplaying span").width();
     $("#api_lfm_current_song1").width(wrapper - playing);
 }
 
-$(
-    function () {
-        let aud = $('audio')[0];
-        aud.src = 'https://stream.laut.fm/' + station;
-        aud.title = sender + ' - LIVE';
-        $('.playbtn').on('click', function () {
-            if (aud.paused) {
-                aud.play();
-                $('.play-pause').removeClass('fa-play').addClass('fa-stop');
-            } else {
-                aud.pause();
-                $('.play-pause').removeClass('fa-stop').addClass('fa-play');
-            }
+function audiobtn() {
+    let aud = $('audio')[0];
+    aud.src = 'https://stream.laut.fm/' + station;
+    aud.title = sender + ' - LIVE';
+    $('.playbtn').on('click', function () {
+        if (aud.paused) {
+            aud.play();
+            $('.play-pause').removeClass('fa-play').addClass('fa-stop');
+        } else {
+            aud.pause();
+            $('.play-pause').removeClass('fa-stop').addClass('fa-play');
+        }
 
-        })
-    }
-);
-
-
+    })
+}
